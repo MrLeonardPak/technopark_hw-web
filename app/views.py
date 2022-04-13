0e200b47155a2191a02c0e5b4ffa620a9f192a48
Leonard Pak <leopak2000@gmail.com>
2022-04-13 20:12:06 +0300

    Fix some bugs

--- app/views.py
+++ app/views.py
@@ -2,40 +2,6 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from app.models import Question, Answer, Tag, Profile, LikeForQuestion, LikeForAnswer
 
-QUESTIONS = [
-    {
-        "title": f"Question #{i}",
-        "text": '''
-                I ate dinner.
-                There is always a next time.
-                In the end, we all felt like we ate too much.
-                We all agreed; it was a magnificent evening.
-                I'm confident that I'll win the tennis match.
-                She opened the door.
-                The car turned the corner.
-                Nothing beats a complete sentence.
-                She advised him to see a lawyer, so he did.
-                Once you know all the elements, it's not difficult to pull together a sentence. ,
-                ''',
-        "id": i
-    } for i in range(40)
-]
-
-ANSWERS = [
-    {
-        "text": f'''
-                This is text for answer #{i}.
-                I don't want to fail the test.
-                There is always a next time.
-                You were late, weren't you ?
-                He denied knowing anything about their plans.
-                No one can make you feel inferior without your consent.
-                '''
-    } for i in range(7)
-]
-
-TAGS = ["bla"*i for i in range(1, 5)]
-
 
 def index(request):
     questions = paginate(Question.objects.new_questions(), request)
